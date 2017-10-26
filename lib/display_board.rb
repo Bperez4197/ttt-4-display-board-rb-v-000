14091ebf6517143275f3627cfc17be22d95778e4
Bperez4197 <bryceperez31@gmail.com>
2017-10-26 09:07:41 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = ["O","O","O","O","O","X","O","O","O"]
-def display_board(board = [5])
-  puts "   |   |   ","-----------","   | #{board} |   ","-----------","   |   |   "
+def display_board(board)
+  puts "   |   |   ","-----------","   | #{board[5]} |   ","-----------","   |   |   "
  "A Tic Tac Toe Board"
 end
 display_board(board)
